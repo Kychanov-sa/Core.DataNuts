4f9fad971795fe0b97eb69e72e1b983bc56663a2
Stanislav Kychanov <kychanov_sa@directum.ru>
2025-08-20 22:01:47 +0400

    Описание формата.

---
+++ src/Core.DataNuts/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace GlacialBytes.Core.DataNuts;
+
+/// <summary>
+/// Заголовок ореха данных.
+/// </summary>
+public struct DataNutHeader
+{
+  public UInt32 Signature;
+  public UInt16 Version;
+  public UInt64 CoreHash;
+  public UInt32 CoreSize;
+  public UInt16 CoreType;
+}
